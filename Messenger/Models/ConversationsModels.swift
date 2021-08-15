dda52441ca2e58e768ae02ce951ed64edf66082a
Yasuk3 <56111339+Yasuk3@users.noreply.github.com>
2021-08-15 23:54:58 +0100

    Add files via upload

---
+++ Messenger/Models/ConversationsModels.swift
@@ -0,0 +1,17 @@
+
+//
+
+import Foundation
+
+struct Conversation {
+    let id: String
+    let name: String
+    let otherUserEmail: String
+    let latestMessage: LatestMessage
+}
+
+struct LatestMessage {
+    let date: String
+    let text: String
+    let isRead: Bool
+}
